98adda1bac075fe1e45cd1cca5ff1cbde39ac8e8
Shift <shift@laravelshift.com>
2024-12-16 13:50:49 +0000

    Adopt anonymous migrations

--- database/migrations/2020_08_28_093618_create_borrows_table.php
+++ database/migrations/2020_08_28_093618_create_borrows_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateBorrowsTable extends Migration
+return new class extends Migration
 {
     /**
      * Run the migrations.
@@ -35,4 +35,4 @@ class CreateBorrowsTable extends Migration
     {
         Schema::dropIfExists('borrows');
     }
-}
+};
