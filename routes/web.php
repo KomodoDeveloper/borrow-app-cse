736aa4e2604fa54ac85a4b9c3daef25e3d56c624
Shift <shift@laravelshift.com>
2024-12-16 13:50:36 +0000

    Adopt class based routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,13 @@
 <?php
 
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\BorrowController;
+use App\Http\Controllers\CatalogController;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\EquipmentController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\MyBorrowController;
+use App\Http\Controllers\StateOfBorrowController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,55 +25,55 @@ Route::get('/', function () {
     return view('home');
 });
 */
-Route::get('/', 'HomeController@index')->name('home');
-Route::get('/login', 'HomeController@login')->name('login');
-Route::get('/logout', 'HomeController@logout')->name('logout');
+Route::get('/', [HomeController::class, 'index'])->name('home');
+Route::get('/login', [HomeController::class, 'login'])->name('login');
+Route::get('/logout', [HomeController::class, 'logout'])->name('logout');
 
-Route::get('/admin', 'AdminController@index')->name('admin.index');
-Route::get('/scan', 'AdminController@scan')->name('admin.scan');
-Route::post('/details', 'AdminController@getElementByCode')->name('admin.getElementByCode');
-Route::get('/archive/borrows', 'AdminController@archiveBorrowsIndex')->name('admin.archive');
-Route::get('/listinventorycollaborators/', 'AdminController@listInventoryCollaborators')->name('admin.listinventorycollab');
-Route::get('/listinventorymultimedia/', 'AdminController@listInventoryMultimedia')->name('admin.listinventorymultimedia');
+Route::get('/admin', [AdminController::class, 'index'])->name('admin.index');
+Route::get('/scan', [AdminController::class, 'scan'])->name('admin.scan');
+Route::post('/details', [AdminController::class, 'getElementByCode'])->name('admin.getElementByCode');
+Route::get('/archive/borrows', [AdminController::class, 'archiveBorrowsIndex'])->name('admin.archive');
+Route::get('/listinventorycollaborators/', [AdminController::class, 'listInventoryCollaborators'])->name('admin.listinventorycollab');
+Route::get('/listinventorymultimedia/', [AdminController::class, 'listInventoryMultimedia'])->name('admin.listinventorymultimedia');
 
-Route::get('/catalog', 'CatalogController@index')->name('catalog.index');
-Route::get('/catalog/{category_option}', 'CatalogController@getCategory')->name('catalog.getCategory');
-Route::get('/intern/catalog', 'CatalogController@indexIntern')->name('catalog.indexIntern');
-Route::get('/intern/catalog/{category_option}', 'CatalogController@internGetCategory')->name('catalog.internGetCategory');
+Route::get('/catalog', [CatalogController::class, 'index'])->name('catalog.index');
+Route::get('/catalog/{category_option}', [CatalogController::class, 'getCategory'])->name('catalog.getCategory');
+Route::get('/intern/catalog', [CatalogController::class, 'indexIntern'])->name('catalog.indexIntern');
+Route::get('/intern/catalog/{category_option}', [CatalogController::class, 'internGetCategory'])->name('catalog.internGetCategory');
 
-Route::get('/myborrows', 'MyBorrowController@index')->name('myborrow.index');
+Route::get('/myborrows', [MyBorrowController::class, 'index'])->name('myborrow.index');
 
-Route::get('/stateofborrow', 'StateOfBorrowController@index')->name('stateofborrow.index');
-Route::get('/stateofborrow/{borrow_id}/return', 'StateOfBorrowController@returnBorrow')->name('stateofborrow.return');
-Route::put('/stateofborrow/updateavailability', 'StateOfBorrowController@updateAvailability')->name('stateofborrow.updateavailability');
-Route::get('/stateofborrow/{borrow_id}/delete', 'StateOfBorrowController@destroy')->name('stateofborrow.delete');
-Route::get('/stateofborrow/tocontrol', 'StateOfBorrowController@tocontrolindex')->name('stateofborrow.tocontrolindex');
-Route::get('/stateofborrow/{borow_id}/close', 'StateOfBorrowController@archivedBorrow')->name('stateofborrow.close');
+Route::get('/stateofborrow', [StateOfBorrowController::class, 'index'])->name('stateofborrow.index');
+Route::get('/stateofborrow/{borrow_id}/return', [StateOfBorrowController::class, 'returnBorrow'])->name('stateofborrow.return');
+Route::put('/stateofborrow/updateavailability', [StateOfBorrowController::class, 'updateAvailability'])->name('stateofborrow.updateavailability');
+Route::get('/stateofborrow/{borrow_id}/delete', [StateOfBorrowController::class, 'destroy'])->name('stateofborrow.delete');
+Route::get('/stateofborrow/tocontrol', [StateOfBorrowController::class, 'tocontrolindex'])->name('stateofborrow.tocontrolindex');
+Route::get('/stateofborrow/{borow_id}/close', [StateOfBorrowController::class, 'archivedBorrow'])->name('stateofborrow.close');
 
-Route::get('/borrow/new', 'BorrowController@create')->name('borrow.create');
-Route::get('/borrow/many', 'BorrowController@createmany')->name('borrow.createmany');
-Route::post('/borrow/store', 'BorrowController@store')->name('borrow.store');
-Route::post('/borrow/storemany', 'BorrowController@storemany')->name('borrow.storemany');
-Route::get('/borrow/new/{equipment_id}', 'BorrowController@customcreate')->name('borrow.customcreate');
-Route::get('/borrow/{id}/edit', 'BorrowController@edit')->name('borrow.edit');
-Route::put('/borrow/{id}/update', 'BorrowController@update')->name('borrow.update');
-Route::post('/borrow/new/planned', 'BorrowController@planned')->name('borrow.planned');
+Route::get('/borrow/new', [BorrowController::class, 'create'])->name('borrow.create');
+Route::get('/borrow/many', [BorrowController::class, 'createmany'])->name('borrow.createmany');
+Route::post('/borrow/store', [BorrowController::class, 'store'])->name('borrow.store');
+Route::post('/borrow/storemany', [BorrowController::class, 'storemany'])->name('borrow.storemany');
+Route::get('/borrow/new/{equipment_id}', [BorrowController::class, 'customcreate'])->name('borrow.customcreate');
+Route::get('/borrow/{id}/edit', [BorrowController::class, 'edit'])->name('borrow.edit');
+Route::put('/borrow/{id}/update', [BorrowController::class, 'update'])->name('borrow.update');
+Route::post('/borrow/new/planned', [BorrowController::class, 'planned'])->name('borrow.planned');
 
-Route::get('/equipment/add', 'EquipmentController@create')->name('equipment.create');
-Route::post('/equipment/store', 'EquipmentController@store')->name('equipment.store');
-Route::get('/equipment/{equipment_id}', 'EquipmentController@show')->name('equipment.show');
-Route::get('/equipment/{id}/edit', 'EquipmentController@edit')->name('equipment.edit');
-Route::put('/equipment/{id}/update', 'EquipmentController@update')->name('equipment.update');
-Route::get('/equipment/{id}/destroy', 'EquipmentController@destroy')->name('equipment.destroy');
-Route::get('/equipment/{id}/duplicate', 'EquipmentController@duplicate')->name('equipment.duplicate');
-Route::post('/equipment/{id}/storeCopy', 'EquipmentController@storeCopy')->name('equipment.storeCopy');
+Route::get('/equipment/add', [EquipmentController::class, 'create'])->name('equipment.create');
+Route::post('/equipment/store', [EquipmentController::class, 'store'])->name('equipment.store');
+Route::get('/equipment/{equipment_id}', [EquipmentController::class, 'show'])->name('equipment.show');
+Route::get('/equipment/{id}/edit', [EquipmentController::class, 'edit'])->name('equipment.edit');
+Route::put('/equipment/{id}/update', [EquipmentController::class, 'update'])->name('equipment.update');
+Route::get('/equipment/{id}/destroy', [EquipmentController::class, 'destroy'])->name('equipment.destroy');
+Route::get('/equipment/{id}/duplicate', [EquipmentController::class, 'duplicate'])->name('equipment.duplicate');
+Route::post('/equipment/{id}/storeCopy', [EquipmentController::class, 'storeCopy'])->name('equipment.storeCopy');
 
-Route::get('/category/manager', 'CategoryController@index')->name('category.index');
-Route::get('/category/add', 'CategoryController@create')->name('category.create');
-Route::post('/category/store', 'CategoryController@store')->name('category.store');
-Route::get('/category/{id}/edit', 'CategoryController@edit')->name('category.edit');
-Route::put('/category/{id}/update', 'CategoryController@update')->name('category.update');
-Route::get('/category/{id}/destroy', 'CategoryController@destroy')->name('category.destroy');
+Route::get('/category/manager', [CategoryController::class, 'index'])->name('category.index');
+Route::get('/category/add', [CategoryController::class, 'create'])->name('category.create');
+Route::post('/category/store', [CategoryController::class, 'store'])->name('category.store');
+Route::get('/category/{id}/edit', [CategoryController::class, 'edit'])->name('category.edit');
+Route::put('/category/{id}/update', [CategoryController::class, 'update'])->name('category.update');
+Route::get('/category/{id}/destroy', [CategoryController::class, 'destroy'])->name('category.destroy');
 
 Route::get('/contact', function () {
     return view('contact');
